85d71827522540f247271acbd5911daf8e6c6863
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-02-21 01:06:10 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -4,64 +4,64 @@
 
 document.querySelector("[rel='icon']").href = "https://sfwx.github.io/image/icon/mcstructure.png";
 
-function fwxBuildItem() {
-  if (typeof fwx.item.template !== "object") {
-    alert("Não foi possível carregar item.js");
+fwx.buildItem = function () {
+  if (typeof this.item.template !== "object") {
+    fwx.log("error", "Não foi possível carregar item.js", true);
     return;
   }
-  const json = structuredClone(itemJson);
-  const item = json.value.structure.value.entities.value.value[0];
-  item.Item.value.Count.value = Math.min(Math.max(Number(document.getElementById("count").value), 1), 64);
-  item.Item.value.Name.value = document.getElementById("itemId").value || "minecraft:diamond";
-  if (document.getElementById("displayName").value.trim()) {
-    item.Item.value.tag.value.display.value.Name.value = document.getElementById("displayName").value;
+  const fwx.item.json = structuredClone(this.item.template);
+  const item = fwx.item.json.value.structure.value.entities.value.value[0];
+  item.Item.value.Count.value = Math.min(Math.max(Number(document.getElementById("fwxCount").value), 1), 64);
+  item.Item.value.Name.value = document.getElementById("fwxItemId").value || "minecraft:diamond";
+  if (document.getElementById("fwxDisplayName").value.trim()) {
+    item.Item.value.tag.value.display.value.Name.value = document.getElementById("fwxDisplayName").value;
   }
   else {
     delete item.Item.value.tag.value.display.value.Name;
   }
-  if (document.getElementById("lore").value.trim()) {
-    item.Item.value.tag.value.display.value.Lore.value.value = document.getElementById("lore").value.trim().split("\n");
+  if (document.getElementById("fwxLore").value.trim()) {
+    item.Item.value.tag.value.display.value.Lore.value.value = document.getElementById("fwxLore").value.trim().split("\n");
   }
   else {
     delete item.Item.value.tag.value.display.value.Lore;
   }
-  if (!document.getElementById("displayName").value.trim() && !document.getElementById("lore").value.trim()) {
+  if (!document.getElementById("fwxDisplayName").value.trim() && !document.getElementById("fwxLore").value.trim()) {
     delete item.Item.value.tag.value.display;
   }
   try {
-    if (!document.getElementById("ench").value.trim()) {
+    if (!document.getElementById("fwxEnch").value.trim()) {
 delete item.Item.value.tag.value.ench;
     }
     else {
-item.Item.value.tag.value.ench.value.value = JSON.parse(document.getElementById("ench").value);
+item.Item.value.tag.value.ench.value.value = JSON.parse(document.getElementById("fwxEnch").value);
 if (!item.Item.value.tag.value.ench.value.value.length) {
   delete item.Item.value.tag.value.ench;
 }
     }
   }
   catch {
-    alert("Json de encantamentos inválido.");
+    fwx.log("warn", "Json de encantamentos inválido.", true);
     return;
   }
-  if (!Number(document.getElementById("unbreakable").value)) {
+  if (!Number(document.getElementById("fwxUnbreakable").value)) {
     delete item.Item.value.tag.value.Unbreakable;
   }
   if (!item.Item.value.tag.value.display && !item.Item.value.tag.value.ench && !item.Item.value.tag.value.Unbreakable) {
     delete item.Item.value.tag;
   }
-  if (document.getElementById("tags").value.trim()) {
-    item.Tags.value.value = document.getElementById("tags").value.trim().split(", ");
+  if (document.getElementById("fwxTags").value.trim()) {
+    item.Tags.value.value = document.getElementById("fwxTags").value.trim().split(", ");
   }
   else {
     item.Tags.value.value = [ "FwX404" ];
   }
-  if (document.getElementById("customName").value.trim()) {
-    item.CustomName.value = document.getElementById("customName").value;
+  if (document.getElementById("fwxCustomName").value.trim()) {
+    item.CustomName.value = document.getElementById("fwxCustomName").value;
   }
   else {
     delete item.CustomName;
   }
-  return json;
+  return fwx.item.json;
 }
 
 /* Todos os direitos são reservados */
