b9af77bc962f8f75be6be0e79e22de49b3266f98
FlowniX <92480881+sfwx@users.noreply.github.com>
2026-02-26 17:26:56 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ fwx.item.build = function () {
     fwx.log("error", "Não foi possível carregar item.js", true);
     return;
   }
+  fwx.log("info", "Clonando estrutura JSON do item..");
   this.json = structuredClone(this.template);
   const item = this.json.value.structure.value.entities.value.value[0];
+  fwx.log("info", "Gerando nova estrutura JSON com valores atualizados..");
   item.Item.value.Count.value = Math.min(Math.max(Number(document.getElementById("fwxCount").value), 1), 64);
   item.Item.value.Damage.value = Math.max(Number(document.getElementById("fwxDamage").value), 0);
   item.Item.value.Name.value = document.getElementById("fwxItemId").value || "minecraft:diamond";
@@ -21,24 +23,29 @@ fwx.item.build = function () {
   }
   else {
     delete item.Item.value.tag.value.display.value.Name;
+    fwx.log("warn", "\"Display Name\" foi removido da estrutura.");
   }
   if (document.getElementById("fwxLore").value.trim()) {
     item.Item.value.tag.value.display.value.Lore.value.value = document.getElementById("fwxLore").value.trim().split("\n");
   }
   else {
     delete item.Item.value.tag.value.display.value.Lore;
+    fwx.log("warn", "\"Display Lore\" foi removido da estrutura.");
   }
   if (!document.getElementById("fwxDisplayName").value.trim() && !document.getElementById("fwxLore").value.trim()) {
     delete item.Item.value.tag.value.display;
+    fwx.log("warn", "\"Display\" foi removido por !!!!!.");
   }
   try {
     if (!document.getElementById("fwxEnch").value.trim()) {
       delete item.Item.value.tag.value.ench;
+      fwx.log("warn", "\"Ench\" foi removido por !!!!!.");
     }
     else {
       item.Item.value.tag.value.ench.value.value = JSON.parse(document.getElementById("fwxEnch").value);
       if (!item.Item.value.tag.value.ench.value.value.length) {
         delete item.Item.value.tag.value.ench;
+        fwx.log("warn", "\"Ench\" foi removido por !!!!!.");
       }
     }
   }
@@ -48,9 +55,11 @@ fwx.item.build = function () {
   }
   if (!Number(document.getElementById("fwxUnbreakable").value)) {
     delete item.Item.value.tag.value.Unbreakable;
+    fwx.log("warn", "\"Unbreakable\" foi removido por !!!!!.");
   }
   if (!item.Item.value.tag.value.display && !item.Item.value.tag.value.ench && !item.Item.value.tag.value.Unbreakable) {
     delete item.Item.value.tag;
+    fwx.log("warn", "\"Tag\" foi removido por !!!!!.");
   }
   if (document.getElementById("fwxTags").value.trim()) {
     item.Tags.value.value = document.getElementById("fwxTags").value.trim().split(", ");
@@ -63,6 +72,7 @@ fwx.item.build = function () {
   }
   else {
     delete item.CustomName;
+    fwx.log("warn", "\"Custom Name\" foi removido por !!!!!.");
   }
   return this.json;
 }
